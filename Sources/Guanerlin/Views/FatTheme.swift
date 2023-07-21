67cf25344c378ad426f7e89866d129f83056f2f6
chengyaojin <7008596+guanerlin@users.noreply.github.com>
2023-07-21 20:47:33 +0800

    clear files

--- Sources/Guanerlin/Views/FatTheme.swift
+++ Sources/Guanerlin/Views/FatTheme.swift
@@ -371,6 +371,23 @@ extension Node where Context == HTML.BodyContext {
 
 }
 
+let gittalk: String =
+    """
+    <script>
+        const gitalk = new Gitalk({
+          clientID: 'GitHub Application Client ID',
+          clientSecret: 'GitHub Application Client Secret',
+          repo: 'GitHub repo',      // The repository of store comments,
+          owner: 'GitHub repo owner',
+          admin: ['GitHub repo owner and collaborators, only these guys can initialize github issues'],
+          id: location.pathname,      // Ensure uniqueness and length less than 50
+          distractionFreeMode: false  // Facebook-like distraction free mode
+        })
+
+        gitalk.render('gitalk-container')
+    </script>
+    """
+
 let googleAndBaidu: String =
     """
     <script>
